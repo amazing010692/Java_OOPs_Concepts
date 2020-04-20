cf6c4ec43e0af4063db2c00d3d08220d9ecf89d6
Janielle Gregorio <hello_janielle1992@yahoo.com.ph>
2020-04-20 21:09:30 +0800

    Inheritance

---
+++ src/inheritance/BullDog.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class BullDog extends Dog {
+	
+	public static void main(String[] args) {
+		
+		BullDog obj = new BullDog();
+		obj.sound();
+	}
+
+}
---
+++ src/inheritance/Dog.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class Dog extends Animal {
+	
+	public static void main(String[] args) {
+		
+		Dog obj = new Dog();
+		obj.sound();
+	}
+	
+
+}
