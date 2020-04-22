011dd1a78d6d337875afd57934ce44741fdc7946
Janielle Gregorio <hello_janielle1992@yahoo.com.ph>
2020-04-22 17:44:36 +0800

    Polymorphism - Overloading | Static Binding | Early Binding | Compile Time Polymorphism

---
+++ src/polymorphism/BPI.java
@@ -0,0 +1,17 @@
+package polymorphism;
+
+public class BPI {
+	
+	public void getHomeLoanROI(String bankName, int amount) {
+		
+	}
+	
+	public void getHomeLoanROI(int amount, String bankName) {
+		
+	}
+	
+	public void getHomeLoanROI() {
+		
+	}
+
+}
