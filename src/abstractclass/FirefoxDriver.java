526422871e570abde710d37a6b605964b702c390
Janielle Gregorio <hello_janielle1992@yahoo.com.ph>
2020-04-25 16:13:37 +0800

    Abstract classes

---
+++ src/abstractclass/FirefoxDriver.java
@@ -0,0 +1,8 @@
+package abstractclass;
+
+public abstract class FirefoxDriver extends WebDriver {
+	
+	public abstract void getFirefoxInstance();
+
+
+}
---
+++ src/abstractclass/Test.java
@@ -0,0 +1,13 @@
+package abstractclass;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		WebDriver c = new ChildFirefox();
+		c.captureScreenshot();
+		
+
+	}
+
+}
