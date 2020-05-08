7be0b8eee3d016d048df5fa8a0a15f70a3e346d4
Janielle Joy Gregorio <hello_janielle1992@yahoo.com.ph>
2020-05-08 16:07:08 +0800

    Reflection API

---
+++ src/reflectionExample/Home.java
@@ -0,0 +1,65 @@
+package reflectionExample;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+public class Home {
+
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		Class clazz = t.getClass();
+		System.out.println(clazz.getSimpleName());
+		Constructor[] cons = clazz.getDeclaredConstructors();
+		System.out.println(cons.length);
+		
+		for(Constructor con : cons) {
+			
+			System.out.println(con.getName());			
+		}
+		Method[] arrayOfMethods = clazz.getDeclaredMethods();
+		
+		System.out.println(clazz.getMethods().length);
+		System.out.println(arrayOfMethods.length);
+		
+		for(Method m : arrayOfMethods) {
+			
+			System.out.println(m.getName() + "---Return Type is: --" + m.getReturnType());
+			
+			Parameter[] params = m.getParameters();
+			
+			for(Parameter param : params) {
+				
+				System.out.println(param.getName());
+			}
+		}
+		
+		/*
+		 * Test --> Object --> Class object
+		 * method --> get executed --> Method
+		 * constructor --> called ---> Constructor
+		 * 
+		 * Log in lof files - log4j
+		 * log.debug(doLogin test executed)
+		 * log.debug(doUserReg test executed)
+		 * 
+		 * @BeforeMethod
+		 * public void beforeMethod(Method m) {
+		 * 
+		 * 		(Test case executed is: + m.getName)
+		 * }
+		 * 
+		 * @Test
+		 * doLogin(String username, String password) {
+		 * 
+		 * }
+		 * 
+		 * @Test
+		 * doUserReg() {
+		 * 
+		 * }
+		 */
+	}
+
+}
